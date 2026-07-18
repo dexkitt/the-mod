4133a3e3a527ff43218a9ac90008e6c00f465ed2
dexkitt <189269364+dexkitt@users.noreply.github.com>
2026-07-18 00:21:28 -0700

    added some log messages and other stuff

--- src/main/java/net/dmws/DexSModWithStuff.java
+++ src/main/java/net/dmws/DexSModWithStuff.java
@@ -1,30 +1,39 @@
 package net.dmws;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.minecraft.resources.Identifier;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class DexSModWithStuff implements ModInitializer {
-	public static final String MOD_ID = "dmws";
+  public static final String ModId = "dmws";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+  // This logger is used to write text to the console and the log file.
+  // It is considered best practice to use your mod id as the logger's name.
+  // That way, it's clear which mod wrote info, warnings, and errors.
+  public static final Logger Logger = LoggerFactory.getLogger(ModId);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+  public static Identifier id(String path) {
+    return Identifier.fromNamespaceAndPath(ModId, path);
+  }
 
-		LOGGER.info("Hello Fabric world!");
-	}
+  @Override
+  public void onInitialize() {
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
+    // However, some things (like resources) may still be uninitialized.
+    // Proceed with mild caution.
 
-	public static Identifier id(String path) {
-		return Identifier.fromNamespaceAndPath(MOD_ID, path);
-	}
+    final int LogRandom = (new Random()).nextInt(1, 5);
+    final String LogMessage;
+
+    if (LogRandom == 1) LogMessage = "hello....";
+    else if (LogRandom == 2) LogMessage = "dont get too comfortable because 🎶nothing grows in comfort🎶";
+    else if (LogRandom == 3) LogMessage = "folk valley";
+    else if (LogRandom == 4) LogMessage = "no";
+    else LogMessage = "ok";
+
+    Logger.info(LogMessage);
+  }
 }
