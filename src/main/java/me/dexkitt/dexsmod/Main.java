7ac39c1a00523dc9bd6ffbad0c3ac4fa97ad56e2
dexkitt <189269364+dexkitt@users.noreply.github.com>
2026-07-18 08:44:56 -0700

    new one cause old one died!

---
+++ src/main/java/me/dexkitt/dexsmod/Main.java
@@ -0,0 +1,10 @@
+package me.dexkitt.dexsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+  @Override
+  public void onInitialize() {
+  }
+}
---
+++ src/main/java/me/dexkitt/dexsmod/client/MainClient.java
@@ -0,0 +1,10 @@
+package me.dexkitt.dexsmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {
+  }
+}
